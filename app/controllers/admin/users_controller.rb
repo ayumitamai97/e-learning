510d34645f1d81d1caec16f2ca8064c3402dce65
ayumitamai97 <ayumitamai97@gmail.com>
2018-09-05 01:49:58 +0900

    Rearranged views related to admin/users_controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,27 +1,45 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
+    @users = User.all
   end
 
-  def new
-  end
-
-  def create
+  def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to admin_users_path, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def destroy
-  end
-
-  def show
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_users_path, notice: 'User was successfully destroyed.' }
+    end
   end
 
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
-      #TODO: strong parameter
+      params.require(:user)
+        .permit(:email, :encrypted_password, :reset_password_token, :confirmation_token, :unconfirmed_email, :avatar)
+    end
+
+    def authorize
+      redirect_to root_path unless current_user.admin?
     end
 end
