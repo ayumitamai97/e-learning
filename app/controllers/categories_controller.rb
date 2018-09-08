4476fda2a22acc999ee9e399f591cb2b0db45adc
ayumitamai97 <ayumitamai97@gmail.com>
2018-09-08 16:53:48 +0900

    Redirect to admin_root if user is admin and current path is root

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
+    redirect_to admin_root_path if current_user && current_user.admin?
     @categories = Category.all
   end
 
