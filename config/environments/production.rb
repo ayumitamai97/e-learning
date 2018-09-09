99505af162389fc990205e3f010455709a23ac05
ayumitamai97 <ayumitamai97@gmail.com>
2018-09-09 22:49:36 +0900

    Modified typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method       = :stmp
+  config.action_mailer.delivery_method       = :smtp
   config.action_mailer.default_url_options   = { host: 'ayumi-elearning.herokuapp.com' }
   ActionMailer::Base.smtp_settings           = {
       address:              'smtp.sendgrid.net',
