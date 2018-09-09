6379019a0d4e6e794a19b29c641d0bf0730488d8
ayumitamai97 <ayumitamai97@gmail.com>
2018-09-09 18:46:09 +0900

    Added users' seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@ User.create!(name: "Taro Yamada",
             password: "super_strong_password",
             password_confirmation: "super_strong_password",
             admin: true)
+
+100.times do |time|
+  User.create!(name: "User" + time.to_s,
+              email: "email" + time.to_s + "@example.com",
+              password: "password",
+              password_confirmation: "password",
+              admin: false)
+end
